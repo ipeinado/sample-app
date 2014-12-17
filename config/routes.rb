a98e1cd0ce96f96292b30fc75b21123b100b8afe
Ignacio Peinado <i.peinado.martinez@gmail.com>
2014-12-17 20:25:07 +0100

    up to 8.4

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'users/new'
-
   root 'static_pages#home'
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
@@ -10,6 +8,10 @@ Rails.application.routes.draw do
 
   resources :users
 
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
